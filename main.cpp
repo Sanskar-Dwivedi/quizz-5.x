fc37c629b71b1b9cadf397e95446227f57d8b9d8
Sanskar-Dwivedi <sanskardwivedi835@gmail.com>
2025-07-21 17:05:29 +0530

    code is now beautiful

--- main.cpp
+++ main.cpp
@@ -2,32 +2,48 @@
 #include <string>
 #include <string_view>
 
-// OOh god this is really messy
+std::string nameInput(int num)
+{
+    std::cout << "Enter the name of person " << num << ":";
+    std::string name;
+    std::getline(std::cin >> std::ws, name);
+    return name;
+}
 
-int main(){
-    std::cout<<"Enter the name of person #1: ";
-    std::string name1{};
-    std::getline(std::cin>>std::ws, name1);
+int ageInput(std::string_view name)
+{
 
-    std::cout<<"Enter the age of "<<name1<<": ";
-    int age1{};
-    std::cin>>age1;
+    std::cout << "Enter the age of " << name << ":";
+    int age{};
+    std::cin >> age;
+    return age;
+}
 
-    std::cout<<"Enter the name of person #2: ";
-    std::string name2{};
-    std::getline(std::cin>>std::ws, name2);
+void result(int age1, int age2, std::string_view name1, std::string_view name2)
+{
 
-    std::cout<<"Enter the age of "<<name2<<": ";
-    int age2{};
-    std::cin>>age2;
-
-    if (age1>age2)
-        std::cout<<name1<<" (age "<<age1<<") is older than "<<name2<<" (age "<<age2<<").";
-    else if (age2>age1)
-    
-        std::cout<<name2<<" (age "<<age2<<") is older than "<<name1<<" (age "<<age1<<").";
+    if (age1 > age2)
+    {
+        std::cout << name1 << " (age " << age1 << ") is older than " << name2 << " (age " << age2 << ")" << "\n";
+    }
+    else if (age2 > age1)
+    {
+        std::cout << name2 << " (age " << age2 << ") is older than " << name1 << " (age " << age1 << ")" << "\n";
+    }
     else
-        std::cout<<"both are of same age";
+        std::cout << "thay are both of the same age\n";
     
+    
+}
+
+int main()
+{
+
+    std::string name1{nameInput(1)};
+    int age1{ageInput(name1)};
+    std::string name2{nameInput(2)};
+    int age2{ageInput(name2)};
+    result(age1, age2, name1, name2);
+
     return 0;
 }
